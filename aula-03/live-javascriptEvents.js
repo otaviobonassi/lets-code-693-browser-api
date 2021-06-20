43eacf1a1e64da2eab41890d9a35b54afab6461c
otaviobonassi <obonassi@gmail.com>
2021-06-20 16:12:56 -0300

    starting class 03

---
+++ aula-03/live-javascriptEvents.js
@@ -0,0 +1,17 @@
+// CONSTRÓI ---> SELECIONA ---> ALTERAR DINAMICAMENTE
+
+// SELEÇÃO DE ELEMENTOS
+
+//getElementById
+//querySelector  ---> querySelector('div'), querySelector('.class'), querySelector('#id')
+//getElementByClassName
+//querySelectorAll
+//getElementByName
+//getElementByTagName (p, div)
+
+// MANIPULANDO O DOM
+
+//append e appendChild
+//createElement
+//classList (add, remove, toggle)
+//element.style.______
