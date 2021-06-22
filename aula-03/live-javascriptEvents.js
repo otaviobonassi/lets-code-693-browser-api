5813045c2063e95c6bcb324310e163c6c183455e
otaviobonassi <obonassi@gmail.com>
2021-06-21 22:20:19 -0300

    class finished

--- aula-03/live-javascriptEvents.js
+++ aula-03/live-javascriptEvents.js
@@ -15,3 +15,41 @@
 //createElement
 //classList (add, remove, toggle)
 //element.style.______
+
+// Eventos
+
+// Relacionados a um comp.: focus, blur, input, change
+const div = document.querySelector(".container")
+div.addEventListener('click', () => {
+    console.log("cliquei no pai")
+})
+const input = document.querySelector('input')
+input.addEventListener('focus', (evt) => {
+    console.log(evt)
+    input.style.backgroundColor = 'grey'
+})
+input.addEventListener('click', (evt) => {
+    console.log("cliquei no filho")
+    // evt.stopPropagation()
+})
+
+// Relacionados ao teclado: keydown, keypress, keyup
+input.addEventListener('keypress', (evt) => {
+    if (evt.key === 'Enter') console.log("seu pedido é uma ordem")
+})
+
+// Relacionados ao mouse: click, mousedown, mouseup, mouseenter, mouseover, mouseleave
+input.addEventListener('mouseleave', (evt) => {
+    console.log("mouseleave")
+})
+
+// Relacionados ao form: submit
+const form = document.addEventListener('submit', (evt) => {
+    evt.preventDefault()
+    console.log("enviei")
+})
+
+// const typo = (evt) => {
+//     evt.preventDefault()
+//     console.log("enviei")
+// }
