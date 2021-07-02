6c319ce5ffb6eadf7fedf830e59f92fb9ae30126
otaviobonassi <obonassi@gmail.com>
2021-07-01 21:11:19 -0300

    starting class

---
+++ aula-08/live-AJAX.js
@@ -0,0 +1,10 @@
+//  Seleção de Elementos
+//  Manipulação de Elementos
+//  Eventos no Navegador
+//  cookies vs localStorage vs sessionStorage
+
+// AJAX - Asynchronous Javascript and XML
+
+// Javascript É UMA LINGUAGEM SINGLE THREADED
+// - Using XMLHttpRequest
+// - Using Fetch
